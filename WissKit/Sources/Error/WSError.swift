863471502ad71f465ba6adfccace106613d88080
Alan <cheolwoo.baek@navercorp.com>
2020-02-04 17:55:44 +0900

    Define WSError and WSHttpError

---
+++ WissKit/Sources/Error/WSError.swift
@@ -0,0 +1,42 @@
+//
+// Created by Alan on 2020/02/04.
+// Copyright (c) 2020 Wiss. All rights reserved.
+//
+
+
+import Foundation
+
+
+protocol WSError: LocalizedError {
+
+    static var unknown: Self { get }
+
+    var code: Int { get }
+    var message: String? { get }
+
+    init(code: Int, message: String?)
+
+}
+
+
+extension WSError {
+
+    init(_ error: Error) {
+        if let wsError = error as? Self {
+            self.init(code: wsError.code, message: wsError.message)
+        } else {
+            self.init(code: Self.unknown.code, message: error.localizedDescription)
+        }
+    }
+
+
+    var errorDescription: String? {
+        self.message
+    }
+
+
+    static func ~=(match: Self, error: Error) -> Bool {
+        match.code == Self(error).code
+    }
+
+}
