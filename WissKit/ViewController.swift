d97f7ae0cb9c757077bbdf2d1234bf7d3f2c58ae
Alan <cheolwoo.baek@navercorp.com>
2020-02-07 20:09:56 +0900

    Implement RenderingSize (not completed)

--- WissKit/ViewController.swift
+++ WissKit/ViewController.swift
@@ -15,3 +15,55 @@ class ViewController: UIViewController {
     }
 
 }
+
+
+public struct RenderingSize {
+
+    public let width: CGFloat
+    public let height: CGFloat
+
+
+    init(width: CGFloat, height: CGFloat) {
+        self.width = width
+        self.height = height
+    }
+
+
+    init(size: CGSize) {
+        self.width = size.width
+        self.height = size.height
+    }
+
+}
+
+
+public struct RenderingSizeConstraints {
+
+    var width: (CGFloat) -> Bool
+    var height: (CGFloat) -> Bool
+
+
+    init(widthConstraints: @escaping (CGFloat) -> Bool = { _ in true }, heightConstraints: @escaping (CGFloat) -> Bool = { _ in true }) {
+        self.width = widthConstraints
+        self.height = heightConstraints
+    }
+
+}
+
+
+public class RenderingSizeConstraintsMaker {
+
+    var constraints = RenderingSizeConstraints()
+
+    func width(_ relation: @escaping (CGFloat, CGFloat) -> Bool, _ value: CGFloat) -> Self {
+        self.constraints.width = { relation($0, value) }
+        return self
+    }
+
+
+    func height(_ relation: @escaping (CGFloat, CGFloat) -> Bool, _ value: CGFloat) -> Self {
+        self.constraints.width = { relation($0, value) }
+        return self
+    }
+
+}
