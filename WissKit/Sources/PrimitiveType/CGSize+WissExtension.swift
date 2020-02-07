14b31d5cd0233bf7a347222cb02767db2c8889ea
Alan <cheolwoo.baek@navercorp.com>
2020-02-07 19:43:19 +0900

    Add CGSize+WissExtension

---
+++ WissKit/Sources/PrimitiveType/CGSize+WissExtension.swift
@@ -0,0 +1,27 @@
+//
+// Created by Alan on 2020/02/07.
+// Copyright (c) 2020 Wiss. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+
+extension CGSize {
+
+    public static func * (scalar: CGFloat, size: CGSize) -> CGSize {
+        CGSize(width: scalar * size.width, height: scalar * size.height)
+    }
+
+
+    public static func * (size: CGSize, scalar: CGFloat) -> CGSize {
+        scalar * size
+    }
+
+
+    public static func *= (size: inout CGSize, scalar: CGFloat) {
+        size = size * scalar
+    }
+
+}
