5aa7fb292a8d2dfde64a178f76dd424e1f8e6fa6
Alan <cheolwoo.baek@navercorp.com>
2020-02-21 16:27:27 +0900

    Add the feature applying some attributes to NSMutableAttributedString

--- WissKit/Source/PrimitiveType/NSMutableAttributedString.swift
+++ WissKit/Source/PrimitiveType/NSMutableAttributedString.swift
@@ -10,6 +10,50 @@ import UIKit
 
 extension Wiss where WissBase: NSMutableAttributedString {
 
+    public func add(font: UIFont, range: NSRange? = nil) {
+        let actualRange = range ?? self.base.string.wiss.wholeNsRange
+        self.base.addAttribute(.font, value: font, range: actualRange)
+    }
+
+
+    public func add(textColor: UIColor, range: NSRange? = nil) {
+        let actualRange = range ?? self.base.string.wiss.wholeNsRange
+        self.base.addAttribute(.foregroundColor, value: textColor, range: actualRange)
+    }
+
+
+    public func add(alignment: NSTextAlignment, range: NSRange? = nil) {
+        var actualRange = range ?? self.base.string.wiss.wholeNsRange
+        let paragraphStyle: NSMutableParagraphStyle
+
+        if let style = self.base.attribute(.paragraphStyle, at: actualRange.location, effectiveRange: &actualRange) as? NSParagraphStyle,
+           let mutableStyle = style.mutableCopy() as? NSMutableParagraphStyle {
+            paragraphStyle = mutableStyle
+        } else {
+            paragraphStyle = NSMutableParagraphStyle()
+        }
+
+        paragraphStyle.alignment = alignment
+        self.base.addAttribute(.paragraphStyle, value: paragraphStyle, range: actualRange)
+    }
+
+
+    public func add(lineBreakMode: NSLineBreakMode, range: NSRange? = nil) {
+        var actualRange = range ?? self.base.string.wiss.wholeNsRange
+        let paragraphStyle: NSMutableParagraphStyle
+
+        if let style = self.base.attribute(.paragraphStyle, at: actualRange.location, effectiveRange: &actualRange) as? NSParagraphStyle,
+           let mutableStyle = style.mutableCopy() as? NSMutableParagraphStyle {
+            paragraphStyle = mutableStyle
+        } else {
+            paragraphStyle = NSMutableParagraphStyle()
+        }
+
+        paragraphStyle.lineBreakMode = lineBreakMode
+        self.base.addAttribute(.paragraphStyle, value: paragraphStyle, range: actualRange)
+    }
+
+
     public func add(lineHeight: CGFloat, range: NSRange? = nil) {
         var actualRange = range ?? self.base.string.wiss.wholeNsRange
         let paragraphStyle: NSMutableParagraphStyle
@@ -22,7 +66,7 @@ extension Wiss where WissBase: NSMutableAttributedString {
         }
 
         paragraphStyle.lineHeightMultiple = self.base.wiss.lineHeightMultiple(for: lineHeight)
-        self.base.setAttributes([.paragraphStyle: paragraphStyle], range: actualRange)
+        self.base.addAttribute(.paragraphStyle, value: paragraphStyle, range: actualRange)
     }
 
 }
