6cceba92a4286954e4cc2d717e82dd900820e308
Alan <cheolwoo.baek@navercorp.com>
2020-03-24 00:28:11 +0900

    Fix errors

--- WissKit/Sources/Wiss/UIFont+Wiss.swift
+++ WissKit/Sources/Wiss/UIFont+Wiss.swift
@@ -16,7 +16,6 @@ extension UIFont: WissCompatible {}
 extension Wiss where WissBase: UIFont {
 
     public static func italicFont(ofSize fontSize: CGFloat, degree: CGFloat = 13) -> UIFont {
-        UIColor.red
         let matrix = CGAffineTransform(a: 1, b: 0, c: tan(degree * .pi / 180), d: 1, tx: 0, ty: 0)
         let descriptor = UIFontDescriptor(name: "", matrix: matrix)
         return UIFont(descriptor: descriptor, size: fontSize)
